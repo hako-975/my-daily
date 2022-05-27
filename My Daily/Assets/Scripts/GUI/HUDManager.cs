18c1b6348a8b1ddb2bcd9014a684b8b95e50d98e
hako-975 <andrifirmansaputra1@gmail.com>
2022-05-28 00:22:50 +0700

    ultah milda

--- My Daily/Assets/Scripts/GUI/HUDManager.cs
+++ My Daily/Assets/Scripts/GUI/HUDManager.cs
@@ -5,22 +5,48 @@ using UnityEngine;
 public class HUDManager : MonoBehaviour
 {
     public GameObject pausedPanel;
+    public AudioSource musicBGM;
+
+    private bool isPaused = false;
 
     public void OnClickPausedButton()
     {
+        isPaused = true;
         Time.timeScale = 0;
+        musicBGM.volume = 0;
+        musicBGM.Pause();
+        musicBGM.time -= 2f;
         pausedPanel.SetActive(true);
     }
 
     public void OnClickResumeButton()
     {
+        isPaused = false;
         Time.timeScale = 1;
+        musicBGM.Play();
+        musicBGM.volume = 1;
         pausedPanel.SetActive(false);
     }
 
     public void OnClickQuitButton()
     {
+        isPaused = false;
         Application.Quit();
         Debug.Log("Quit Game!");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                OnClickResumeButton();
+            }
+            else
+            {
+                OnClickPausedButton();
+            }
+        }
     }
 }
